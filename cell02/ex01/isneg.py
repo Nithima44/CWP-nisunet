32c0382bd10dacc639f72144c38299ca0f58ca2b
Nithima <nitima7859@gmail.com>
2026-09-21 14:27:12 +0700

    cell2

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+Num=int(input("Enter an Integer: "))
+if Num<0:
+    print("This number is negative")
+elif Num==0:
+    print("This number is equal to zero")
+else:
+    print("This number is positive")
